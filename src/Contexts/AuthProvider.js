8f16b90c3c9562ffc05c0597f335d7558f833db1
mohammedtakbir <takbirhossain678@gmail.com>
2022-11-08 18:59:22 +0600

    google user authenticaiton added for log in and sign up

--- src/Contexts/AuthProvider.js
+++ src/Contexts/AuthProvider.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 import { createContext } from 'react';
 import { useState } from 'react';
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext();
-const auth = getAuth(app)
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
@@ -18,13 +19,18 @@ const AuthProvider = ({ children }) => {
     //* login an user
     const userLogin = (email, password) => {
         return signInWithEmailAndPassword(auth, email, password);
+    };
+    //* sign in with google
+    const googleSignIn = () => {
+        return signInWithPopup(auth, googleProvider);
     }
 
     const getAuth = {
         user,
         loading,
+        userLogin,
         userSignup,
-        userLogin
+        googleSignIn,
     };
     return (
         <AuthContext.Provider value={getAuth}>
